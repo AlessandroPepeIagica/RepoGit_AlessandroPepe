1b58f6b0805c6feaac08943d61e9432b1ab828dc
AlessandroPepe <alessandro.pepe@iagica.it>
2025-02-27 17:50:17 +0100

    modificato file testgit 2 main

--- src/main/java/testGit1.java
+++
@@ -1,4 +0,0 @@
-public class testGit1 {
-    int x = 1;
-    int z = 3;
-  }
--- src/main/java/testGit1_AlessandroPepe.java
+++ src/main/java/testGit1_AlessandroPepe.java
@@ -1,4 +1,4 @@
-public class testGit1 {
+public class testGit1_AlessandroPepe {
     int x = 1;
-    int y = 2;
+    int z = 3;
   }
